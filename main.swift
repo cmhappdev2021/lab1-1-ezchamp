ca58b501e73836de2b24ab894cc6c7dfb2b4e4d5
ezchamp <emile269@gmail.com>
2020-08-31 18:26:24 +0000

    Added print statements

--- main.swift
+++ main.swift
@@ -7,8 +7,11 @@ print("How to use playgrounds to make writing Swift fun and simple")
 
 // Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the print function to display the song title and artist.
 
-
+print("Hotel California By Eagles")
 
 // Use multiple print functions to write out some of the lyrics to the song.
 
-
+print("On a dark desert highway")
+print("Cool wind in my hair")
+print("Warm smell of colitas")
+print("Rising up through the air") 
